76ffe7c44cac830cba1ed588bb6c185c378f1a8d
Ghaousse <h.ghaousse@laposte.net>
2020-08-06 10:11:18 +0200

    addbdd

---
+++ gbaf.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 06 août 2020 à 08:10
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gbaf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acteur`
+--
+
+DROP TABLE IF EXISTS `acteur`;
+CREATE TABLE IF NOT EXISTS `acteur` (
+  `id_acteur` int(11) NOT NULL AUTO_INCREMENT,
+  `acteur` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `logo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_acteur`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `acteur`
+--
+
+INSERT INTO `acteur` (`id_acteur`, `acteur`, `description`, `logo`) VALUES
+(1, 'Formation&co', 'Formation&co est une association française présente sur tout le territoire.\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.\r\nNotre proposition : \r\n- un financement jusqu’à 30 000€ ;\r\n- un suivi personnalisé et gratuit ;\r\n- une lutte acharnée contre les freins sociétaux et les stéréotypes.\r\n\r\nLe financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . Nous collaborons avec des personnes talentueuses et motivées.\r\nVous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! Nos financements s’adressent à tous.', 'png'),
+(2, 'Protectpeople', 'Protectpeople finance la solidarité nationale.\r\nNous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.\r\n\r\nChez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.\r\nProectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.\r\nNous garantissons un accès aux soins et une retraite.\r\nChaque année, nous collectons et répartissons 300 milliards d’euros.\r\nNotre mission est double :\r\nsociale : nous garantissons la fiabilité des données sociales ;\r\néconomique : nous apportons une contribution aux activités économiques.', 'png'),
+(3, 'Dsa France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.\r\nNotre philosophie : s’adapter à chaque entreprise.\r\nNous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises.', 'png'),
+(4, 'CDE', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. \r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 'png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `id_post` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_acteur` int(11) NOT NULL,
+  `date_add` date NOT NULL,
+  `post` text NOT NULL,
+  PRIMARY KEY (`id_post`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `visiteurs`
+--
+
+DROP TABLE IF EXISTS `visiteurs`;
+CREATE TABLE IF NOT EXISTS `visiteurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `reponse` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `visiteurs`
+--
+
+INSERT INTO `visiteurs` (`id`, `nom`, `prenom`, `pseudo`, `password`, `question`, `reponse`) VALUES
+(1, 'a', 'a', 'a', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', 'a', 'a'),
+(2, 'a', 'a', 'a', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', 'a', 'a'),
+(3, 'Jean', 'Dujardin', 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'Couleur de voiture ?', 'Bleu'),
+(4, 'Aiden', 'Pearce', 'WD', '0b7afc3e9363c80f619524194c0d7e4661396ecb', 'Couleur de casquette ?', 'Noir'),
+(5, 'Azerty', 'Luc', 'Lulu', '0f300f33b728cabd2cd5cbde86757722de291ceb', 'Quelle a Ã©tÃ© votre premiÃ¨re voiture ?', 'Clio 4');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vote`
+--
+
+DROP TABLE IF EXISTS `vote`;
+CREATE TABLE IF NOT EXISTS `vote` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `id_acteur` int(11) NOT NULL,
+  `vote` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
